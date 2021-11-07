39426bd732a54107ad201a69a3abd78cb4b30a28
ferueda <fe.rueda@outlook.com>
2021-11-07 18:51:28 -0300

    Create pokemons store

---
+++ pokemons/store.go
@@ -0,0 +1,25 @@
+package pokemons
+
+import "github.com/jinzhu/gorm"
+
+type Store struct {
+	db *gorm.DB
+}
+
+func NewStore(db *gorm.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) GetOne(id uint) *Pokemon {
+	pokemon := &Pokemon{}
+	s.db.First(pokemon, id)
+	return pokemon
+}
+
+func (s *Store) GetAll() []*Pokemon {
+	pokemons := []*Pokemon{}
+	s.db.Find(&pokemons)
+	return pokemons
+}
