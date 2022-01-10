a6ca93ce1eb947b7424c048fd8ec63057e5f9430
ferueda <fe.rueda@outlook.com>
2022-01-09 20:15:05 -0800

    Add Token handler and endpoint

--- pkg/app/handlers.go
+++ pkg/app/handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/ferueda/go-postgres-server-example/pkg/api"
 	"github.com/gorilla/mux"
@@ -23,7 +24,6 @@ func (s *Server) ApiStatus() http.HandlerFunc {
 
 func (s *Server) CreateUser() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
 
 		var newUserReq api.NewUserRequest
 		err := json.NewDecoder(r.Body).Decode(&newUserReq)
@@ -39,6 +39,7 @@ func (s *Server) CreateUser() http.HandlerFunc {
 		}
 
 		w.WriteHeader(http.StatusCreated)
+		w.Header().Add("Content-Type", "application/json")
 		newUserRes := api.NewUserResponse{Model: u.Model, Name: u.Name, Email: u.Email}
 		err = json.NewEncoder(w).Encode(&newUserRes)
 		if err != nil {
@@ -50,7 +51,6 @@ func (s *Server) CreateUser() http.HandlerFunc {
 
 func (s *Server) GetAllPokemons() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
 
 		pokemons, err := s.pokemonService.GetAll()
 		if err != nil {
@@ -59,6 +59,7 @@ func (s *Server) GetAllPokemons() http.HandlerFunc {
 		}
 
 		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
 		err = json.NewEncoder(w).Encode(&pokemons)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -69,7 +70,6 @@ func (s *Server) GetAllPokemons() http.HandlerFunc {
 
 func (s *Server) GetPokemonById() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
 
 		id, err := strconv.ParseUint(mux.Vars(r)["id"], 10, 32)
 		if err != nil {
@@ -84,10 +84,62 @@ func (s *Server) GetPokemonById() http.HandlerFunc {
 		}
 
 		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
 		err = json.NewEncoder(w).Encode(&p)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
+}
+
+func (s *Server) Token() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var tokenReq api.TokenRequest
+
+		err := json.NewDecoder(r.Body).Decode(&tokenReq)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if tokenReq.Email == "" || tokenReq.Password == "" {
+			http.Error(w, "must provide email and password", http.StatusBadRequest)
+			return
+		}
+
+		u, err := s.userService.GetByEmail(tokenReq.Email)
+		if err != nil {
+			http.Error(w, "wrong email or password", http.StatusForbidden)
+			return
+		}
+
+		if err := s.userService.VerifyPassword(u, tokenReq.Password); err != nil {
+			http.Error(w, "wrong email or password", http.StatusForbidden)
+			return
+		}
+
+		token, err := s.userService.CreateToken(u.Email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(&api.TokenResponse{Token: token})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func extractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
 }
--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -1,26 +1,26 @@
 package app
 
 import (
-	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
 func (s *Server) Routes() *mux.Router {
 	r := s.router
 
-	r.HandleFunc("/api/status", s.ApiStatus())
+	v := "v1"
+	base := "/api/" + v
 
-	r.HandleFunc("/api/pokemons", s.GetAllPokemons()).Methods("GET")
-	r.HandleFunc("/api/pokemons/{id:[0-9]+}", s.GetPokemonById()).Methods("GET")
+	r.HandleFunc(base+"/status", s.ApiStatus())
+
+	r.HandleFunc(base+"/pokemons", s.GetAllPokemons()).Methods("GET")
+	r.HandleFunc(base+"/pokemons/{id:[0-9]+}", s.GetPokemonById()).Methods("GET")
 
 	// r.HandleFunc("/users/{id:[0-9]+}", uh.GetOne).Methods("GET")
 	// r.HandleFunc("/users/{id:[0-9]+}/favorites", uh.GetFavorites).Methods("GET")
 	// r.HandleFunc("/users/{id:[0-9]+}/favorites", uh.AddFavorite).Methods("POST")
 
-	// r.HandleFunc("/api/signin", uh.SignUp).Methods("POST")
-	r.HandleFunc("/api/users", s.CreateUser()).Methods("POST")
+	r.HandleFunc(base+"/users", s.CreateUser()).Methods("POST")
+	r.HandleFunc(base+"/users/token", s.Token()).Methods("POST")
 
-	http.Handle("/api", r)
 	return r
 }
