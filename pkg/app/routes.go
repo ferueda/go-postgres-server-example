bb16fc52bbbd6031a4a66f780d8417101a322031
ferueda <fe.rueda@outlook.com>
2022-01-10 21:37:36 -0800

    Add GetFavorites and AddFavorites routes

--- pkg/app/routes.go
+++ pkg/app/routes.go
@@ -15,13 +15,13 @@ func (s *Server) Routes() *mux.Router {
 	r.HandleFunc(base+"/pokemons", s.GetAllPokemons()).Methods("GET")
 	r.HandleFunc(base+"/pokemons/{id:[0-9]+}", s.GetPokemonById()).Methods("GET")
 
-	// r.HandleFunc("/users/{id:[0-9]+}", uh.GetOne).Methods("GET")
-	// r.HandleFunc("/users/{id:[0-9]+}/favorites", uh.GetFavorites).Methods("GET")
-	// r.HandleFunc("/users/{id:[0-9]+}/favorites", uh.AddFavorite).Methods("POST")
-
 	r.HandleFunc(base+"/users", s.CreateUser()).Methods("POST")
 	r.HandleFunc(base+"/users/{id:[0-9]+}", s.Auth(s.GetUserById())).Methods("GET")
+
 	r.HandleFunc(base+"/users/token", s.Token()).Methods("POST")
+
+	r.HandleFunc(base+"/users/{id:[0-9]+}/favorites", s.Auth(s.GetFavorites())).Methods("GET")
+	r.HandleFunc(base+"/users/{id:[0-9]+}/favorites", s.Auth(s.AddFavorite())).Methods("POST")
 
 	return r
 }
